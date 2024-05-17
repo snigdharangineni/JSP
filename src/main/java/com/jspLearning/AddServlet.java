de6d5d0f4a581211b34a30901dafdeb05a987cf3
snigdha <sunny.rangineni@gmail.com>
2024-05-17 16:15:05 -0500

    RequestDispatcher

--- src/main/java/com/jspLearning/AddServlet.java
+++ src/main/java/com/jspLearning/AddServlet.java
@@ -2,20 +2,25 @@ package com.jspLearning;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
 	{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
 		
-		res.getWriter().println("result is " + k);
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
 }
  
---
+++ src/main/java/com/jspLearning/SqServlet.java
@@ -0,0 +1,21 @@
+package com.jspLearning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		PrintWriter out = res.getWriter();
+		
+		int k = (int)req.getAttribute("k");
+		
+		k = k*k;
+		out.println("result is " + k);
+	}
+}
